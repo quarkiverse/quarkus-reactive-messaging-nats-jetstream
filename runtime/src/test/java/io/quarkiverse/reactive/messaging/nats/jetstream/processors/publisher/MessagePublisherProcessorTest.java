f3728918d694a9e48903f2090a2b79c304938ad2
ricmir <ricmir@norlys.dk>
2024-02-23 09:28:49 +0100

    Cleanup

--- runtime/src/test/java/io/quarkiverse/reactive/messaging/nats/jetstream/processors/publisher/MessagePublisherProcessorTest.java
+++ runtime/src/test/java/io/quarkiverse/reactive/messaging/nats/jetstream/processors/publisher/MessagePublisherProcessorTest.java
@@ -30,38 +30,4 @@ public class MessagePublisherProcessorTest {
         assertThat(options.getConsumerConfiguration().getMaxDeliver()).isEqualTo(maxDeliever);
         assertThat(options.getConsumerConfiguration().getBackoff()).hasSize(1);
         assertThat(options.getConsumerConfiguration().getBackoff()).contains(Duration.ofSeconds(1));
-    }
-
-    @Test
-    void testRepeating1() {
-        ExecutorService pullExecutor = Executors.newSingleThreadExecutor(JetstreamWorkerThread::new);
-
-        AtomicInteger count = new AtomicInteger(0);
-        Multi<String> multi = Multi.createBy().repeating().supplier(() -> {
-            String msg = null;
-            try {
-                msg = nextMessage();
-            } catch (InterruptedException ex) {
-            }
-
-            count.incrementAndGet();
-            return msg;
-        })
-                .until(e -> count.get() > 1000)
-                .map(String::toUpperCase)
-                .runSubscriptionOn(pullExecutor)
-                .onTermination().invoke(pullExecutor::shutdownNow);
-
-        multi.subscribe().with(i -> System.out.println(i));
-        System.out.println(count.get());
-    }
-
-    private String nextMessage() throws InterruptedException {
-        if (new Random().nextDouble() > 0.95D) {
-            throw new InterruptedException();
-        } else {
-            return "message-" + UUID.randomUUID();
-        }
-    }
-
 }
