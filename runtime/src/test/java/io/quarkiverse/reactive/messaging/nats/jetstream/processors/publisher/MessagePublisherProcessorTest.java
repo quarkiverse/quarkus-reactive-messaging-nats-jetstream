26975bd8520f1b1728b323863a735aefe243b4d0
ricmir <ricmir@norlys.dk>
2024-02-23 09:36:25 +0100

    Fixed compile

--- runtime/src/test/java/io/quarkiverse/reactive/messaging/nats/jetstream/processors/publisher/MessagePublisherProcessorTest.java
+++ runtime/src/test/java/io/quarkiverse/reactive/messaging/nats/jetstream/processors/publisher/MessagePublisherProcessorTest.java
@@ -1,18 +1,11 @@
 package io.quarkiverse.reactive.messaging.nats.jetstream.processors.publisher;
 
-import static io.quarkiverse.reactive.messaging.nats.jetstream.processors.publisher.MessagePublisherProcessor.createPushSubscribeOptions;
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.time.Duration;
-import java.util.Random;
-import java.util.UUID;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
-
 import org.junit.jupiter.api.Test;
 
-import io.smallrye.mutiny.Multi;
+import java.time.Duration;
+
+import static io.quarkiverse.reactive.messaging.nats.jetstream.processors.publisher.MessagePublisherProcessor.createPushSubscribeOptions;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MessagePublisherProcessorTest {
 
@@ -30,4 +23,5 @@ public class MessagePublisherProcessorTest {
         assertThat(options.getConsumerConfiguration().getMaxDeliver()).isEqualTo(maxDeliever);
         assertThat(options.getConsumerConfiguration().getBackoff()).hasSize(1);
         assertThat(options.getConsumerConfiguration().getBackoff()).contains(Duration.ofSeconds(1));
+    }
 }
