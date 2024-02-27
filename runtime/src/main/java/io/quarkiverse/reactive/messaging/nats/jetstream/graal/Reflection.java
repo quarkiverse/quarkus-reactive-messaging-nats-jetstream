0cd9accec6e9dc039a09759e22ffca8e274ca169
Paw Figgé Kjeldgaard <paw@figge.dk>
2024-02-27 21:36:48 +0100

    Fixed reflection error

---
+++ runtime/src/main/java/io/quarkiverse/reactive/messaging/nats/jetstream/graal/Reflection.java
@@ -0,0 +1,7 @@
+package io.quarkiverse.reactive.messaging.nats.jetstream.graal;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection(targets = { byte[].class, byte.class })
+public class Reflection {
+}
