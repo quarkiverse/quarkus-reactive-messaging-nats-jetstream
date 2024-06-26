b45d794d971547e3da8a6b03740434270c0b3e61
Paw Figgé Kjeldgaard <e-pafikj@norlys.dk>
2024-06-26 14:06:58 +0200

    Fixed getting OpenTelemetry instance

--- runtime/src/main/java/io/quarkiverse/reactive/messaging/nats/jetstream/tracing/JetStreamInstrumenter.java
+++ runtime/src/main/java/io/quarkiverse/reactive/messaging/nats/jetstream/tracing/JetStreamInstrumenter.java
@@ -1,27 +1,35 @@
 package io.quarkiverse.reactive.messaging.nats.jetstream.tracing;
 
-import static io.opentelemetry.instrumentation.api.instrumenter.messaging.MessageOperation.RECEIVE;
-import static io.opentelemetry.instrumentation.api.instrumenter.messaging.MessagingAttributesExtractor.create;
-
-import jakarta.enterprise.context.ApplicationScoped;
-
-import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
 import io.opentelemetry.instrumentation.api.instrumenter.InstrumenterBuilder;
 import io.opentelemetry.instrumentation.api.instrumenter.messaging.MessageOperation;
 import io.opentelemetry.instrumentation.api.instrumenter.messaging.MessagingAttributesExtractor;
 import io.opentelemetry.instrumentation.api.instrumenter.messaging.MessagingAttributesGetter;
 import io.opentelemetry.instrumentation.api.instrumenter.messaging.MessagingSpanNameExtractor;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Instance;
+import jakarta.inject.Inject;
+
+import static io.opentelemetry.instrumentation.api.instrumenter.messaging.MessageOperation.RECEIVE;
+import static io.opentelemetry.instrumentation.api.instrumenter.messaging.MessagingAttributesExtractor.create;
+import static io.smallrye.reactive.messaging.tracing.TracingUtils.getOpenTelemetry;
 
 @ApplicationScoped
 public class JetStreamInstrumenter {
+    private final Instance<OpenTelemetry> openTelemetryInstance;
+
+    @Inject
+    public JetStreamInstrumenter(Instance<OpenTelemetry> openTelemetryInstance) {
+        this.openTelemetryInstance = openTelemetryInstance;
+    }
 
     public Instrumenter<JetStreamTrace, Void> publisher() {
         final var attributesExtractor = new JetStreamTraceAttributesExtractor();
         MessagingAttributesGetter<JetStreamTrace, Void> messagingAttributesGetter = attributesExtractor
                 .getMessagingAttributesGetter();
 
-        InstrumenterBuilder<JetStreamTrace, Void> builder = Instrumenter.builder(GlobalOpenTelemetry.get(),
+        InstrumenterBuilder<JetStreamTrace, Void> builder = Instrumenter.builder(getOpenTelemetry(openTelemetryInstance),
                 "io.smallrye.reactive.messaging.jetstream",
                 MessagingSpanNameExtractor.create(messagingAttributesGetter, MessageOperation.PUBLISH));
 
@@ -34,7 +42,7 @@ public class JetStreamInstrumenter {
         final var attributesExtractor = new JetStreamTraceAttributesExtractor();
         MessagingAttributesGetter<JetStreamTrace, Void> messagingAttributesGetter = attributesExtractor
                 .getMessagingAttributesGetter();
-        InstrumenterBuilder<JetStreamTrace, Void> builder = Instrumenter.builder(GlobalOpenTelemetry.get(),
+        InstrumenterBuilder<JetStreamTrace, Void> builder = Instrumenter.builder(getOpenTelemetry(openTelemetryInstance),
                 "io.smallrye.reactive.messaging.jetstream",
                 MessagingSpanNameExtractor.create(messagingAttributesGetter, RECEIVE));
 
