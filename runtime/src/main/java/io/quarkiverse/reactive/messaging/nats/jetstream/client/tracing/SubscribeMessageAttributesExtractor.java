fd687400178109c020f283d769b4ada8051da13b
Paw Figgé Kjeldgaard <paw@figge.dk>
2025-02-22 10:08:37 +0100

    Bumped Quarkus to version 3.18.3

--- runtime/src/main/java/io/quarkiverse/reactive/messaging/nats/jetstream/client/tracing/SubscribeMessageAttributesExtractor.java
+++ runtime/src/main/java/io/quarkiverse/reactive/messaging/nats/jetstream/client/tracing/SubscribeMessageAttributesExtractor.java
@@ -53,6 +53,11 @@ public class SubscribeMessageAttributesExtractor implements AttributesExtractor<
         }
 
         @Override
+        public String getDestinationKind(SubscribeMessageMetadata metadata) {
+            return "Stream";
+        }
+
+        @Override
         public boolean isTemporaryDestination(SubscribeMessageMetadata metadata) {
             return false;
         }
